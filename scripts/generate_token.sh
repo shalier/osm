fb6b2c6b4d82290d317d6e24f9ea52959ca77155
Shalier Xia <shalierxia@microsoft.com>
2021-02-05 13:47:49 -0500

    debug

--- scripts/generate_token.sh
+++ scripts/generate_token.sh
@@ -12,11 +12,11 @@ expireTime=$(date -d "$expireTime + 600 seconds" +%s)
 # Creates JSON Webtoken (JWT)
 header=$(echo '{ "alg": "RS256", "typ": "JWT" }' | jq -r '(. | @base64)')
 payload=$(echo '{"iss": '"$ID"',"iat": '$issueTime' ,"exp": '$expireTime'}'| jq -r '(. | @base64)'| sed s/\+/-/ | sed -E s/=+$//)
+echo $payload
 echo "$PK" > key
 signature=$(echo -n "$header.$payload" | openssl dgst -sha256 -binary -sign key | openssl enc -base64 | tr -d '\n=' | tr -- '+/' '-_')
 
 # Uses JWT to get Installation Access Token (IAT)
 tokenURL=$(curl -i -X GET -H "Authorization: Bearer $header.$payload.$signature" -H "Accept: application/vnd.github.v3+json" https://api.github.com/app/installations | grep 'access_tokens_url' | awk '{print $2}' | sed -e 's/^"//' -e 's/",$//')
+echo $tokenURL
 token=$(curl -i -X POST -H "Authorization: Bearer $header.$payload.$signature" -H "Accept: application/vnd.github.v3+json" "$tokenURL" | grep 'token'| awk '{print $2}' | sed -e 's/^"//' -e 's/",$//')
-
-echo "GITHUB_TOKEN=$token" >> "$GITHUB_ENV"
