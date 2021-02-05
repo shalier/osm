80004e78517312c33d297688ac6ca0c9b4316cfe
Shalier Xia <shalierxia@microsoft.com>
2021-02-05 12:48:55 -0500

    source

--- scripts/generate_token.sh
+++ scripts/generate_token.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 #shellcheck disable=SC2154
 #shellcheck disable=SC2016
+#shellcheck disable=SC2086
 
 issueTime=$(date +%s)
 expireTime=$(date -d "$expireTime + 600 seconds" +%s)
@@ -14,4 +15,5 @@ signature=$(echo -n "$header.$payload" | openssl dgst -sha256 -binary -sign key 
 # Uses JWT to get Installation Access Token (IAT)
 tokenURL=$(curl -i -X GET -H "Authorization: Bearer $header.$payload.$signature" -H "Accept: application/vnd.github.v3+json" https://api.github.com/app/installations | grep 'access_tokens_url' | awk '{print $2}' | sed -e 's/^"//' -e 's/",$//')
 token=$(curl -i -X POST -H "Authorization: Bearer $header.$payload.$signature" -H "Accept: application/vnd.github.v3+json" "$tokenURL" | grep 'token'| awk '{print $2}' | sed -e 's/^"//' -e 's/",$//')
+
 echo "GITHUB_TOKEN=$token" >> "$GITHUB_ENV"
