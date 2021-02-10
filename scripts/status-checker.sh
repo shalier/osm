3c4ba8ca859d1fce939f1f2317a8ee3b4abc7dad
Shalier Xia <shalierxia@microsoft.com>
2021-02-10 12:34:23 -0500

    fix legacy

--- scripts/status-checker.sh
+++ scripts/status-checker.sh
@@ -4,7 +4,7 @@
 GITHUB_REPOSITORY=$1
 GITHUB_RUN_ID=$2
 
-checkSuiteID=`curl -s https://api.github.com/repos/"$GITHUB_REPOSITORY"/actions/runs/"$GITHUB_RUN_ID" | jq -r '.check_suite_id'`
+checkSuiteID=$(curl -s https://api.github.com/repos/"$GITHUB_REPOSITORY"/actions/runs/"$GITHUB_RUN_ID" | jq -r '.check_suite_id')
 
 e2eStatus=$(curl -i -H "Accept: application/vnd.github.v3+json" https://api.github.com/repos/"$GITHUB_REPOSITORY"/check-suites/"$checkSuiteID"/check-runs | grep -B 15 'e2e (2)' | grep 'conclusion' | awk '{print $2}')
 echo e2e "$e2eStatus"
