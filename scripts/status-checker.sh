457fa1558bde5f006a0210dae61f20800ac8a928
Shalier Xia <shalierxia@microsoft.com>
2021-02-10 16:22:49 -0500

    debugs

--- scripts/status-checker.sh
+++ scripts/status-checker.sh
@@ -7,7 +7,10 @@ GITHUB_RUN_ID=$2
 checkSuiteID=$(curl -s https://api.github.com/repos/"$GITHUB_REPOSITORY"/actions/runs/"$GITHUB_RUN_ID" | jq -r '.check_suite_id')
 
 headSHA=$(curl -i -H "Accept: application/vnd.github.v3+json" https://api.github.com/repos/"$GITHUB_REPOSITORY"/check-suites/"$checkSuiteID"/check-runs | grep head_sha| awk '{print $2}'| sed -e 's/^"//' -e 's/",$//')
-
+echo repo: "$GITHUB_REPOSITORY"
+echo sha: "$headSHA"
+curl -i -H "Accept: application/vnd.github.v3+json" https://api.github.com/repos/"$GITHUB_REPOSITORY"/commits/"$headSHA"/check-runs | grep -B 15 'e2e (2)'
+exit 1
 while [ -z "$e2eStatus" ] || [ "$e2eStatus" == null ]; do
     e2eStatus=$(curl -i -H "Accept: application/vnd.github.v3+json" https://api.github.com/repos/"$GITHUB_REPOSITORY"/commits/"$headSHA"/check-runs | grep -B 15 'e2e (2)' | grep 'conclusion' | awk '{print $2}' | sed 's/[^[:alnum:]]\+//g')
     echo e2eStatus "$e2eStatus"
