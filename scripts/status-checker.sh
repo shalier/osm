3de05b03194827ecc0f61168a6e066ac30b08b31
Shalier Xia <shalierxia@microsoft.com>
2021-02-10 12:27:29 -0500

    fix while loop

--- scripts/status-checker.sh
+++ scripts/status-checker.sh
@@ -5,12 +5,18 @@ GITHUB_REPOSITORY=$1
 GITHUB_RUN_ID=$2
 
 checkSuiteID=`curl -s https://api.github.com/repos/$GITHUB_REPOSITORY/actions/runs/$GITHUB_RUN_ID | jq -r '.check_suite_id'`
+
 e2eStatus=$(curl -i -H "Accept: application/vnd.github.v3+json" https://api.github.com/repos/$GITHUB_REPOSITORY/check-suites/$checkSuiteID/check-runs | grep -B 15 'e2e (2)' | grep 'conclusion' | awk '{print $2}')
 echo e2e $e2eStatus
-while [ -z "$e2eStatus" ]; e2eStatus=$(curl -i -H "Accept: application/vnd.github.v3+json" https://api.github.com/repos/$GITHUB_REPOSITORY/check-suites/$checkSuiteID/check-runs | grep -B 15 'e2e (2)' | grep 'conclusion' | awk '{print $2}'); echo e2eStatus $e2eStatus; done
+
+while [ -z "$e2eStatus" ]; do
+    e2eStatus=$(curl -i -H "Accept: application/vnd.github.v3+json" https://api.github.com/repos/$GITHUB_REPOSITORY/check-suites/$checkSuiteID/check-runs | grep -B 15 'e2e (2)' | grep 'conclusion' | awk '{print $2}')
+    echo e2eStatus $e2eStatus
+done
+
 countNumStatusFail=$(curl -i -H "Accept: application/vnd.github.v3+json" https://api.github.com/repos/$GITHUB_REPOSITORY/check_suites/$checkSuiteID/check-runs | grep 'conclusion' | grep 'failure' | wc -l)
 echo countNumStatusFail $countNumStatusFail
-exit 1
+
 if [ $countNumStatusFail -gt 0 ]; then
     echo "$countNumStatusFail Status Checks Failed "
     exit 1
