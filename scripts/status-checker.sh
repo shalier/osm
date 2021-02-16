429fa219839b68551b97203da7ce60574ea7318b
Shalier Xia <shalierxia@microsoft.com>
2021-02-16 18:50:36 -0500

    fix

--- scripts/status-checker.sh
+++ scripts/status-checker.sh
@@ -11,11 +11,13 @@ curl -s -i -X GET -H "Accept: application/vnd.github.v3+json" https://api.github
     while true; do
         status=$(curl -s -i -X GET -H "Accept: application/vnd.github.v3+json" https://api.github.com/repos/"$GITHUB_REPOSITORY"/actions/runs | grep -B6 -A1 '"status":' | grep -B3 -A4 "$headSHA" | grep -A7 "$check" | grep "status" | awk '{print $2}' | sed -e 's/^"//' -e 's/",$//')
             if [ "$status" == "completed" ]; then
+                echo 'Check completed, checking conclusion'
                 conclusion=$(curl -s -i -X GET -H "Accept: application/vnd.github.v3+json" https://api.github.com/repos/"$GITHUB_REPOSITORY"/actions/runs | grep -B6 -A1 '"status":' | grep -B3 -A4 "$headSHA" | grep -A7 "$check" |grep "conclusion"| awk '{print $2}' | sed -e 's/^"//' -e 's/",$//')
                 if [ "$conclusion" != "success" ]; then
                     echo 'Not all status checks passed'
                     exit 1
                 else
+                    echo $check workflow has completed successfully
                     break
                 fi
             fi
