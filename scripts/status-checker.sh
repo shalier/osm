e1b52050206bb48b140ea8a68c90b2846ea4df41
Shalier Xia <shalierxia@microsoft.com>
2021-02-16 17:37:56 -0500

    status checker generalized

--- scripts/status-checker.sh
+++ scripts/status-checker.sh
@@ -7,15 +7,16 @@ GITHUB_RUN_ID=$2
 checkSuiteID=$(curl -s https://api.github.com/repos/"$GITHUB_REPOSITORY"/actions/runs/"$GITHUB_RUN_ID" | jq -r '.check_suite_id')
 headSHA=$(curl -i -H "Accept: application/vnd.github.v3+json" https://api.github.com/repos/"$GITHUB_REPOSITORY"/check-suites/"$checkSuiteID"/check-runs | grep head_sha| awk '{print $2}'| sed -e 's/^"//' -e 's/",$//')
 while true; do
-    status=$(curl -s -i -X GET -H "Accept: application/vnd.github.v3+json" https://api.github.com/repos/"$GITHUB_REPOSITORY"/actions/runs | grep -B6 -A1 '"status":' | grep -B3 -A4 "$headSHA" | grep -A7 "Go" | grep "status" | awk '{print $2}' | sed -e 's/^"//' -e 's/",$//')
-    if [ "$status" == "completed" ]; then
-        conclusion=$(curl -s -i -X GET -H "Accept: application/vnd.github.v3+json" https://api.github.com/repos/"$GITHUB_REPOSITORY"/actions/runs | grep -B6 -A1 '"status":' | grep -B3 -A4 "$headSHA" | grep -A7 "Go" |grep "conclusion"| awk '{print $2}' | sed -e 's/^"//' -e 's/",$//')
-        if [ "$conclusion" == "success" ]; then
-            echo 'All status checks passed'
-            break
-        else
-            echo 'Not all status checks passed'
-            exit 1
+    curl -s -i -X GET -H "Accept: application/vnd.github.v3+json" https://api.github.com/repos/"$GITHUB_REPOSITORY"/actions/runs | grep -B6 -A1 '"status":' | grep -B3 -A4 "$headSHA" | grep name | awk '{print $2}' | grep -v automerge | sed -e 's/^"//' -e 's/",$//' | while read -r check; do
+    status=$(curl -s -i -X GET -H "Accept: application/vnd.github.v3+json" https://api.github.com/repos/"$GITHUB_REPOSITORY"/actions/runs | grep -B6 -A1 '"status":' | grep -B3 -A4 "$headSHA" | grep -A7 "$check" | grep "status" | awk '{print $2}' | sed -e 's/^"//' -e 's/",$//')
+        if [ "$status" == "completed" ]; then
+            conclusion=$(curl -s -i -X GET -H "Accept: application/vnd.github.v3+json" https://api.github.com/repos/"$GITHUB_REPOSITORY"/actions/runs | grep -B6 -A1 '"status":' | grep -B3 -A4 "$headSHA" | grep -A7 "$check" |grep "conclusion"| awk '{print $2}' | sed -e 's/^"//' -e 's/",$//')
+            if [ "$conclusion" != "success" ]; then
+                echo 'Not all status checks passed'
+                exit 1
+            fi
         fi
-    fi
+    done
+    echo 'All status checks passed'
+    break
 done
