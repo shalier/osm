f719f35bed9a746747885884fa7e962f8d0a485d
Shalier Xia <shalierxia@microsoft.com>
2021-02-17 17:49:27 -0500

    try diff labels

--- scripts/status-checker.sh
+++ scripts/status-checker.sh
@@ -6,6 +6,7 @@ GITHUB_RUN_ID=$2
 
 checkSuiteID=$(curl -s https://api.github.com/repos/"$GITHUB_REPOSITORY"/actions/runs/"$GITHUB_RUN_ID" | jq -r '.check_suite_id')
 headSHA=$(curl -i -H "Accept: application/vnd.github.v3+json" https://api.github.com/repos/"$GITHUB_REPOSITORY"/check-suites/"$checkSuiteID"/check-runs | grep head_sha| awk '{print $2}'| sed -e 's/^"//' -e 's/",$//')
+
 curl -s -i -X GET -H "Accept: application/vnd.github.v3+json" https://api.github.com/repos/"$GITHUB_REPOSITORY"/actions/runs | grep -B6 -A1 '"status":' | grep -B3 -A4 "$headSHA" | grep name | awk '{print $2}' | grep -v automerge | sed -e 's/^"//' -e 's/",$//' | while read -r check; do
     while true; do
         status=$(curl -s -i -X GET -H "Accept: application/vnd.github.v3+json" https://api.github.com/repos/"$GITHUB_REPOSITORY"/actions/runs | grep -B6 -A1 '"status":' | grep -B3 -A4 "$headSHA" | grep -A7 "$check" | grep "status" | awk '{print $2}' | sed -e 's/^"//' -e 's/",$//')
@@ -20,13 +21,13 @@ curl -s -i -X GET -H "Accept: application/vnd.github.v3+json" https://api.github
                     break
                 fi
             fi
-        sleep 300
-        treeID=$(curl -s -i -X GET -H "Accept: application/vnd.github.v3+json" https://api.github.com/repos/"$GITHUB_REPOSITORY"/actions/runs | grep -A1 "$headSHA" | grep tree_id | awk '{print $2}' | sed -e 's/^"//' -e 's/",$//' | sort --unique)
-        echo "$treeID"
-        checkIfRebased=$(curl -s -i -X GET -H "Accept: application/vnd.github.v3+json" https://api.github.com/repos/"$GITHUB_REPOSITORY"/actions/runs | grep -A4 "$treeID" | grep "automergetest\[bot\]" | awk '{print $2}' | sed -e 's/^"//' -e 's/",$//')
-        if [  "$checkIfRebased" == "automergetest\[bot\]" ]; then
-            echo Ending this job as PR has been rebased
-            exit 1
+        sleep 100
+        # treeID=$(curl -s -i -X GET -H "Accept: application/vnd.github.v3+json" https://api.github.com/repos/"$GITHUB_REPOSITORY"/actions/runs | grep -A1 "$headSHA" | grep tree_id | awk '{print $2}' | sed -e 's/^"//' -e 's/",$//' | sort --unique)
+        # echo "$treeID"
+        # checkIfRebased=$(curl -s -i -X GET -H "Accept: application/vnd.github.v3+json" https://api.github.com/repos/"$GITHUB_REPOSITORY"/actions/runs | grep -A4 "$treeID" | grep "automergetest\[bot\]" | awk '{print $2}' | sed -e 's/^"//' -e 's/",$//')
+        # if [  "$checkIfRebased" == "automergetest\[bot\]" ]; then
+        #     echo Ending this job as PR has been rebased
+        #     exit 1
         fi
     done
 done
