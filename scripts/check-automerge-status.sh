a450232736c5f2ee39a1ccc63395ce086cb747fb
Shalier Xia <shalierxia@microsoft.com>
2021-02-24 15:03:19 -0500

    use dispatch

--- scripts/check-automerge-status.sh
+++ scripts/check-automerge-status.sh
@@ -2,12 +2,12 @@
 # shellcheck disable=SC1091
 
 GITHUB_REPOSITORY=$1
-prNumber=$2
+PR_NUM=$2
 
 while true; do
-    mergeable=$(curl -i -H "Accept: application/vnd.github.v3+json" https://api.github.com/repos/"$GITHUB_REPOSITORY"/pulls/"$prNumber" | grep -w mergeable | awk '{print $2}' | sed -e 's/,//g')
+    mergeable=$(curl -i -H "Accept: application/vnd.github.v3+json" https://api.github.com/repos/"$GITHUB_REPOSITORY"/pulls/"$PR_NUM" | grep -w mergeable | awk '{print $2}' | sed -e 's/,//g')
     if [ "$mergeable" != "null" ]; then
-        merged=$(curl -i -H "Accept: application/vnd.github.v3+json" https://api.github.com/repos/"$GITHUB_REPOSITORY"/pulls/"$prNumber" | grep -w merged | awk '{print $2}' | sed -e 's/,//g')
+        merged=$(curl -i -H "Accept: application/vnd.github.v3+json" https://api.github.com/repos/"$GITHUB_REPOSITORY"/pulls/"$PR_NUM" | grep -w merged | awk '{print $2}' | sed -e 's/,//g')
         if [ "$merged" == false ]; then
             exit 1
         else
--- scripts/check-if-rebased.sh
+++ scripts/check-if-rebased.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 # shellcheck disable=SC1091
-
+# shellcheck disable=SC2086
 
 GITHUB_REPOSITORY=$1
-prNumber=$2
-ref = $3
+PR_NUM=$2
+REF=$3
 GITHUB_RUN_ID=$4
 
-rebaseable=$(curl -i -H "Accept: application/vnd.github.v3+json" https://api.github.com/repos/"$GITHUB_REPOSITORY"/pulls/"$prNumber" | grep -w rebaseable | awk '{print $2}' | sed -e 's/,//g')
+rebaseable=$(curl -i -H "Accept: application/vnd.github.v3+json" https://api.github.com/repos/"$GITHUB_REPOSITORY"/pulls/"$PR_NUM" | grep -w rebaseable | awk '{print $2}' | sed -e 's/,//g')
 workflowID=$(curl -s https://api.github.com/repos/"$GITHUB_REPOSITORY"/actions/runs/"$GITHUB_RUN_ID" | jq -r '.workflow_id')
 if [ "$rebaseable" == "true" ]; then
-    curl -X POST -H "Accept: application/vnd.github.v3+json" https://api.github.com/repos/"$GITHUB_REPOSITORY"/actions/workflows/"$workflowID"/dispatches -d '{"ref":"'$ref'", "inputs": {"name": "Re-run status checks"}}'
+    curl -X POST -H "Accept: application/vnd.github.v3+json" https://api.github.com/repos/"$GITHUB_REPOSITORY"/actions/workflows/"$workflowID"/dispatches -d '{"ref":"'$REF'", "inputs": {"name": "Re-run status checks"}}'
 fi
