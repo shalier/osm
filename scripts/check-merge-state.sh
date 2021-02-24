b55fa35509b4ea19ec199506d7c186f06e40f4d4
Shalier Xia <shalierxia@microsoft.com>
2021-02-24 08:08:34 -0500

    fix

--- scripts/check-merge-state.sh
+++ scripts/check-merge-state.sh
@@ -6,7 +6,7 @@ prNumber=$1
 while true; do
     mergeable=$(curl -i -H "Accept: application/vnd.github.v3+json" https://api.github.com/repos/shalier/osm/pulls/"$prNumber" | grep -w mergeable | awk '{print $2}' | sed -e 's/,//g')
     if [ "$mergeable" != "null" ]; then
-        merged=${curl -i -H "Accept: application/vnd.github.v3+json" https://api.github.com/repos/shalier/osm/pulls/"$prNumber" | grep -w merged | awk '{print $2}' | sed -e 's/,//g'}
+        merged=$(curl -i -H "Accept: application/vnd.github.v3+json" https://api.github.com/repos/shalier/osm/pulls/"$prNumber" | grep -w merged | awk '{print $2}' | sed -e 's/,//g')
         if [ "$merged" == false ]; then exit 1; fi
     fi
 done
