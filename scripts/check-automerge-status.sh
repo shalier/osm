f0f07ae991566600e1ed3bb1b88ef6f9f565182d
Shalier Xia <shalierxia@microsoft.com>
2021-02-26 00:57:01 -0500

    close-open token13 wait on merged field

--- scripts/check-automerge-status.sh
+++ scripts/check-automerge-status.sh
@@ -5,15 +5,15 @@ GITHUB_REPOSITORY=$1
 PR_NUM=$2
 
 while true; do
-    mergeable=$(curl -i -H "Accept: application/vnd.github.v3+json" https://api.github.com/repos/"$GITHUB_REPOSITORY"/pulls/"$PR_NUM" | grep -w mergeable | awk '{print $2}' | sed -e 's/,//g')
-    echo mergeable "$mergeable"
-    if [ "$mergeable" != "null" ] && [ ! -z "$mergeable" ]; then
-        merged=$(curl -i -H "Accept: application/vnd.github.v3+json" https://api.github.com/repos/"$GITHUB_REPOSITORY"/pulls/"$PR_NUM" | grep -w merged | awk '{print $2}' | sed -e 's/,//g')
-        echo merged "$merged"
-        if [ "$merged" == false ]; then
-            exit 1
-        else
-            break
-        fi
+    # mergeable=$(curl -i -H "Accept: application/vnd.github.v3+json" https://api.github.com/repos/"$GITHUB_REPOSITORY"/pulls/"$PR_NUM" | grep -w mergeable | awk '{print $2}' | sed -e 's/,//g')
+    echo pr: "$PR_NUM"
+    # echo mergeable "$mergeable"
+    merged=$(curl -i -H "Accept: application/vnd.github.v3+json" https://api.github.com/repos/"$GITHUB_REPOSITORY"/pulls/"$PR_NUM" | grep -w merged | awk '{print $2}' | sed -e 's/,//g')
+    echo merged "$merged"
+    if [ "$merged" == false ]; then
+        exit 1
+    else if [ "$merged" == true ]; then
+        break
     fi
+    sleep 30
 done
