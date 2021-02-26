7eba542b00db646d382f46da0aa70a68cefa9791
Shalier Xia <shalierxia@microsoft.com>
2021-02-26 02:45:08 -0500

    close-open token22 check status

--- scripts/check-automerge-status.sh
+++ scripts/check-automerge-status.sh
@@ -7,7 +7,16 @@ PR_NUM=$2
 while true; do
     merged=$(curl -s -i -H "Accept: application/vnd.github.v3+json" https://api.github.com/repos/"$GITHUB_REPOSITORY"/pulls/"$PR_NUM" | grep -w merged | awk '{print $2}' | sed -e 's/,//g')
     if [ "$merged" == false ]; then
-        exit 1
+        while [ -z "$rebaseable" ] || [ "$rebaseable" == "null" ]; do
+            rebaseable=$(curl -s -i -H "Accept: application/vnd.github.v3+json" https://api.github.com/repos/"$GITHUB_REPOSITORY"/pulls/"$PR_NUM" | grep -w rebaseable | awk '{print $2}' | sed -e 's/,//g')
+
+            if [ "$rebaseable" == "true" ]; then
+                break
+            else
+                exit 1
+            fi
+            sleep 30
+        done
     elif [ "$merged" == true ]; then
         break
     fi
