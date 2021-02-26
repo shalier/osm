bce1cb1a6708d918d4f44d8a540fbbd09fbcde39
Shalier Xia <shalierxia@microsoft.com>
2021-02-25 21:59:38 -0500

    close-open token4

--- scripts/check-if-rebased.sh
+++ scripts/check-if-rebased.sh
@@ -10,6 +10,7 @@ rebaseable=$(curl -i -H "Accept: application/vnd.github.v3+json" https://api.git
 echo rebaseable "$rebaseable"
 
 if [ "$rebaseable" == "true" ]; then
-    (curl -X PATCH -H "Authorization Bearer $GITHUB_TOKEN" -H "Accept: application/vnd.github.v3+json" https://api.github.com/repos/"$GITHUB_REPOSITORY"/pulls/"$PR_NUM" -d '{"state":"closed"}'
+    curl -X PATCH -H "Authorization Bearer $GITHUB_TOKEN" -H "Accept: application/vnd.github.v3+json" https://api.github.com/repos/"$GITHUB_REPOSITORY"/pulls/"$PR_NUM" -d '{"state":"closed"}'
+
     curl -X PATCH -H "Authorization Bearer $GITHUB_TOKEN" -H "Accept: application/vnd.github.v3+json" https://api.github.com/repos/"$GITHUB_REPOSITORY"/pulls/"$PR_NUM" -d '{"state":"open"}'
 fi
