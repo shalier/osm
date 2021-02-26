6b341d2ecaa46cb22701a4ba0bfd242cb246f83f
Shalier Xia <shalierxia@microsoft.com>
2021-02-25 19:34:54 -0500

    check run + suite

--- scripts/check-automerge-status.sh
+++ scripts/check-automerge-status.sh
@@ -3,7 +3,7 @@
 
 GITHUB_REPOSITORY=$1
 PR_NUM=$2
-curl -i -H "Accept: application/vnd.github.v3+json" https://api.github.com/repos/"$GITHUB_REPOSITORY"/pulls/"$PR_NUM
+curl -i -H "Accept: application/vnd.github.v3+json" https://api.github.com/repos/"$GITHUB_REPOSITORY"/pulls/"$PR_NUM"
 while true; do
     mergeable=$(curl -i -H "Accept: application/vnd.github.v3+json" https://api.github.com/repos/"$GITHUB_REPOSITORY"/pulls/"$PR_NUM" | grep -w mergeable | awk '{print $2}' | sed -e 's/,//g')
     if [ "$mergeable" != "null" ]; then
