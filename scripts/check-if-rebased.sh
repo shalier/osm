322157a080b48f6b686ab25992524eb74d7d4f71
Shalier Xia <shalierxia@microsoft.com>
2021-02-25 20:26:21 -0500

    close-open

--- scripts/check-if-rebased.sh
+++ scripts/check-if-rebased.sh
@@ -4,17 +4,9 @@
 
 GITHUB_REPOSITORY=$1
 PR_NUM=$2
-REF=$3
-GITHUB_RUN_ID=$4
 
 rebaseable=$(curl -i -H "Accept: application/vnd.github.v3+json" https://api.github.com/repos/"$GITHUB_REPOSITORY"/pulls/"$PR_NUM" | grep -w rebaseable | awk '{print $2}' | sed -e 's/,//g')
-workflowID=$(curl -s https://api.github.com/repos/"$GITHUB_REPOSITORY"/actions/runs/"$GITHUB_RUN_ID" | jq -r '.workflow_id')
 if [ "$rebaseable" == "true" ]; then
-    curl -X POST -H "Accept: application/vnd.github.v3+json" https://api.github.com/repos/"$GITHUB_REPOSITORY"/actions/workflows/"$workflowID"/dispatches -d '{"ref":"'$REF'", "inputs": {"name": "Re-run status checks"}}'
+    curl -X PATCH -H "Accept: application/vnd.github.v3+json" https://api.github.com/repos/"$GITHUB_REPOSITORY"/pulls/"$PR_NUM" -d '{"state":"closed"}'
+    curl -X PATCH -H "Accept: application/vnd.github.v3+json" https://api.github.com/repos/"$GITHUB_REPOSITORY"/pulls/"$PR_NUM" -d '{"state":"open"}'
 fi
-suiteID=$(curl -s https://api.github.com/repos/"$GITHUB_REPOSITORY"/actions/runs/"$GITHUB_RUN_ID" | jq -r '.check_suite_id')
-echo check_run "$suiteID"
-curl -i -H "Accept: application/vnd.github.v3+json" https://api.github.com/repos/"$GITHUB_REPOSITORY"/check-suites/"$suiteID"/check-runs
-echo ""
-echo check_run_run_id "$GITHUB_RUN_ID"
-curl -i -H "Accept: application/vnd.github.v3+json" https://api.github.com/repos/"$GITHUB_REPOSITORY"/check-runs/"$GITHUB_RUN_ID"
