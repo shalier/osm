dd8c49de4bdd038e696530b6b2555fb239ce7fa4
Shalier Xia <shalierxia@microsoft.com>
2021-03-01 21:00:15 -0500

    turn off autorebase

--- scripts/rebase-pull-request.sh
+++ scripts/rebase-pull-request.sh
@@ -13,12 +13,12 @@ while true; do
             rebaseable=$(curl -s -i -H "Accept: application/vnd.github.v3+json" https://api.github.com/repos/"$GITHUB_REPOSITORY"/pulls/"$PR_NUM" | grep -w rebaseable | awk '{print $2}' | sed -e 's/,//g')
 
             if [ "$rebaseable" == "true" ]; then
-                git checkout $PR
-                git checkout -b ${PR}_tmp
-                git rebase $MAIN
-                git checkout $MAIN
-                git pull . ${PR}_tmp
-                git branch -d ${PR}_tmp
+                git checkout "$PR"
+                git checkout -b "${PR}_tmp"
+                git rebase "$MAIN"
+                git checkout "$MAIN"
+                git pull . "${PR}_tmp"
+                git branch -d "${PR}_tmp"
             fi
             sleep 30
         done
